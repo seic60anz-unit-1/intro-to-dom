9327b40e1062343ac0384e2503b6f93cbb09a405
Dido Arellano <dido.arellano@generalassemb.ly>
2022-12-14 16:03:18 +1100

    ...

---
+++ script.js
@@ -0,0 +1,45 @@
+const container = document.querySelector('.container')
+// const listItems = document.querySelectorAll('li')
+const h1InContainer = container.querySelector('h1')
+const img = container.querySelector('img')
+const lists = document.querySelectorAll('ul')
+const li = list.children[1]
+
+// console.log(lists[0].children)
+// console.log(lists[1].children)
+// console.log(li.style)
+
+// console.log(container.children)
+
+// li.style.backgroundColor = 'fuchsia'
+// li.style.color = '#fff'
+
+// li.className = 'selected'
+
+// classList
+// console.log(li.classList)
+// li.classList.add('selected', 'add', 'more', 'infinity')
+// li.classList.remove('add', 'more')
+// li.classList.toggle('selected')
+// li.classList.toggle('newclassname')
+
+// img.src = 'https://placedog.net/700'
+// img.alt = 'Random doggo'
+
+// console.log(li.textContent)
+// li.innerText = 'Hello World'
+// li.innerHTML = 'Hello <strong>World</strong>'
+// li.textContent = 'Hello <strong>World</strong>'
+
+// append, prepend, after, before
+// const newDiv = document.createElement('li')
+// newDiv.textContent = 'New element'
+// console.log(newDiv)
+// container.append(newDiv)
+// container.prepend(newDiv)
+// li.after(newDiv)
+
+// .remove
+// li.remove()
+
+// console.dir(li)
